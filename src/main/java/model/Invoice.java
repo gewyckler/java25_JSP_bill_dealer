73009cbff2c7ac7b39d80ac22ab270e7b39baa90
gewyckler <gewyckler@gmail.com>
2019-09-19 19:14:29 +0200

    Inicialize project

---
+++ src/main/java/model/Invoice.java
@@ -0,0 +1,46 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Formula;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Invoice implements IBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @CreationTimestamp
+    @Column()
+    private LocalDateTime dateOfCreation;
+
+    @Column(nullable = false)
+    private String clientName;
+
+    @Column(nullable = false, length = 10)
+    private String clientNip;
+
+    @Column(nullable = false)
+    private String clientAddress;
+
+    @Column(nullable = false, columnDefinition = " tinyint default 0")
+    private boolean ifPaid;
+    private LocalDateTime dateOfRelease;
+    private LocalDateTime dateOfPayment;
+
+    @Formula(value = "(SELECT SUM(p.price * p.stock) from product p where p.invoice_id = id as billValue)")
+    private Double billValue;
+
+    @OneToMany(mappedBy = "invoice", fetch = FetchType.EAGER)
+//    @JoinColumn(name = "invoice_id")
+    private List<Product> product;
+}
---
+++ src/main/java/model/Product.java
@@ -0,0 +1,35 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.Formula;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product implements IBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private double price;
+
+    @Column(nullable = false)
+    @Formula(value = "(case when (taxType='PRODUCT') then (price*0.23) when (taxType='SERVICES') then price*0.8)end)")
+    private double taxValue;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private TaxType taxType;
+
+    private int stock;
+
+    @ToString.Exclude
+    @ManyToOne()
+    private Invoice invoice;
+}
